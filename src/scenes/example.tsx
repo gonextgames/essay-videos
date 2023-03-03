ea9677b63e9f27a9bf3df716e49a6cc2fb6e94f5
Oliver Barnum <oliverbarnum32@gmail.com>
2023-03-02 22:57:40 -0600

    Create basics

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,32 +1,65 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {Circle, Layout, Text} from '@motion-canvas/2d/lib/components';
+import {Circle, Layout, Text, Line, Rect, Node} from '@motion-canvas/2d/lib/components';
+import {slideTransition} from '@motion-canvas/core/lib/transitions';
+import {
+  all,
+  delay,
+  loop,
+  waitFor,
+  waitUntil,
+} from '@motion-canvas/core/lib/flow';
 import {createRef} from '@motion-canvas/core/lib/utils';
-import {all} from '@motion-canvas/core/lib/flow';
+import {CodeBlock, edit, insert, lines, word} from '@motion-canvas/2d/lib/components/CodeBlock';
+import {Direction, Vector2} from '@motion-canvas/core/lib/types';
 
 export default makeScene2D(function* (view) {
-  const myCircle = createRef<Circle>();
-
-  view.add(
+  const code = createRef<CodeBlock>();
+  const textStyle = {
+    fontWeight: 700,
+    fontFamily: 'JetBrains Mono',
+    fontSize: 32,
+    offsetY: -1,
+    padding: 10,
+    cache: true,
+  };
+  yield view.add(
     <>
-      <Layout width={1920} height={1080} direction={'column'} gap={28} layout padding={20}>
-        
-      </Layout>
-      <Circle
-        ref={myCircle}
-        x={-300}
-        width={240}
-        height={240}
-        fill="#e13238"
-      />,
-      <Text text="Hello" x={-300}
-        width={240}
-        height={240}/>
+      <Rect
+        offset={-1}
+        x={-960 + 80}
+        y={-540 + 80}
+        height={1080 - 160}
+        width={960}
+        clip
+      >
+      <CodeBlock 
+        selection={[
+          [
+            [0, 0],
+            [8, 100],
+          ],
+        ]}
+        ref={code}
+        fontSize={24}
+        lineHeight={36}
+        offsetX={-1}
+        x={-960 / 2}
+        fontFamily={'JetBrains Mono'}
+        code={`templative`} />,
+      </Rect>
     </>
-    
   );
 
-  yield* all(
-    myCircle().position.x(300, 1).to(-300, 1),
-    myCircle().fill('#e6a700', 1).to('#e13238', 1),
-  );
+  yield* slideTransition(Direction.Bottom, 1);
+  yield* waitUntil('showInit');
+  // yield* code().selection(lines(1), 0.3);
+  yield* code().edit(2, false)`templative${insert(` init`)}`;
+  yield* waitUntil("highlightInit")
+  yield* code().selection(word(0, 11, 4), 1);
+  // yield* waitUntil('showProduce');
+  // yield* code().edit(0.8)`templative produce`;
+
+  yield* waitUntil("hold")
+
+
 });
