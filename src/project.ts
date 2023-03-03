9fd7764c3eca41548047ef7174fd7f0832229ccd
Oliver Barnum <oliverbarnum32@gmail.com>
2023-03-03 02:49:02 -0600

    Add audio

--- src/project.ts
+++ src/project.ts
@@ -1,8 +1,9 @@
 import {makeProject} from '@motion-canvas/core/lib';
-
+import introAudio from "./intro.mp3" 
 import example from './scenes/example?scene';
 
 export default makeProject({
   scenes: [example],
   background: '#141414',
+  audio: introAudio,
 });
