01e983e9cb025a1aa28d0fd9771a373b32b346d4
Oliver Barnum <oliverbarnum32@gmail.com>
2023-04-18 22:32:30 -0500

    Rename intro scene

--- essays/caps-and-hammers-hype/src/project.ts
+++ essays/caps-and-hammers-hype/src/project.ts
@@ -1,7 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
 
-import example from './scenes/example?scene';
+import introduction from './scenes/introduction?scene'; 
 
 export default makeProject({
-  scenes: [example],
+  scenes: [introduction],
 });
