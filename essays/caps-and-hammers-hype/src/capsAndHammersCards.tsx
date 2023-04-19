96e9c5b489faac45a2f15368eb32d38a27d435ce
Oliver Barnum <oliverbarnum32@gmail.com>
2023-04-19 00:07:54 -0500

    Add image import

---
+++ essays/caps-and-hammers-hype/src/capsAndHammersCards.tsx
@@ -0,0 +1,51 @@
+import actionBack from "./images/actions-back.jpg"
+import admiral from "./images/actions-admiral.jpg"
+import defector from "./images/actions-admiral.jpg"
+import diplomat from "./images/actions-admiral.jpg"
+import argentina from "./images/actions-admiral.jpg"
+import countryBack from "./images/actions-admiral.jpg"
+import brazil from "./images/actions-admiral.jpg"
+import china from "./images/actions-admiral.jpg"
+import defconOne from "./images/actions-admiral.jpg"
+import defconTwo from "./images/actions-admiral.jpg"
+import defconThree from "./images/actions-admiral.jpg"
+import defconFour from "./images/actions-admiral.jpg"
+import defconFive from "./images/actions-admiral.jpg"
+import defconBack from "./images/actions-admiral.jpg"
+import justification from "./images/actions-admiral.jpg"
+import justificationBack from "./images/actions-admiral.jpg"
+import scienceLabBack from "./images/actions-admiral.jpg"
+import scienceLabReentry from "./images/actions-admiral.jpg"
+import scienceLabSatellites from "./images/actions-admiral.jpg"
+import scienceLabSpaceWalks from "./images/actions-admiral.jpg"
+import scienceLabComputers from "./images/actions-admiral.jpg"
+import scienceLabICBM from "./images/actions-admiral.jpg"
+import scienceLabLanders from "./images/actions-admiral.jpg"
+import scienceLabMoonLanding from "./images/actions-admiral.jpg"
+
+export default {
+    actionBack: actionBack,
+    admiral: admiral,
+    defector: defector,
+    diplomat: diplomat,
+    argentina: argentina,
+    countryBack: countryBack,
+    brazil: brazil,
+    china: china,
+    defconOne: defconOne,
+    defconTwo: defconTwo,
+    defconThree: defconThree,
+    defconFour: defconFour,
+    defconFive: defconFive,
+    defconBack: defconBack,
+    justification: justification,
+    justificationBack: justificationBack,
+    scienceLabBack: scienceLabBack,
+    scienceLabReentry: scienceLabReentry,
+    scienceLabSatellites: scienceLabSatellites,
+    scienceLabSpaceWalks: scienceLabSpaceWalks,
+    scienceLabComputers: scienceLabComputers,
+    scienceLabICBM: scienceLabICBM,
+    scienceLabLanders: scienceLabLanders,
+    scienceLabMoonLanding: scienceLabMoonLanding,
+}
