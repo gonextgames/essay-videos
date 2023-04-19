65ef15b9c857bae89dfed1b2883f1932467ca74d
Oliver Barnum <oliverbarnum32@gmail.com>
2023-04-19 05:46:45 -0500

    Fix action card imports

--- essays/caps-and-hammers-hype/src/capsAndHammersCards.tsx
+++ essays/caps-and-hammers-hype/src/capsAndHammersCards.tsx
@@ -1,11 +1,19 @@
+import argentina from "./images/countries-argentina.jpg"
+import countryBack from "./images/countries-back.jpg"
+import brazil from "./images/countries-brazil.jpg"
+import china from "./images/countries-china.jpg"
+
 import actionBack from "./images/actions-back.jpg"
 import admiral from "./images/actions-admiral.jpg"
-import defector from "./images/actions-admiral.jpg"
-import diplomat from "./images/actions-admiral.jpg"
-import argentina from "./images/actions-admiral.jpg"
-import countryBack from "./images/actions-admiral.jpg"
-import brazil from "./images/actions-admiral.jpg"
-import china from "./images/actions-admiral.jpg"
+import admiralExample from "./images/admiral.png"
+import defector from "./images/actions-defector.jpg"
+import diplomat from "./images/actions-diplomat.jpg"
+import nuke from "./images/actions-nuke.jpg"
+import guerrilla from "./images/actions-guerrilla.jpg"
+import policeman from "./images/actions-policeman.jpg"
+import spy from "./images/actions-spy.jpg"
+import scientist from "./images/actions-scientist.jpg"
+
 import defconOne from "./images/actions-admiral.jpg"
 import defconTwo from "./images/actions-admiral.jpg"
 import defconThree from "./images/actions-admiral.jpg"
@@ -26,11 +34,17 @@ import scienceLabMoonLanding from "./images/actions-admiral.jpg"
 export default {
     actionBack: actionBack,
     admiral: admiral,
+    admiralExample: admiralExample,
     defector: defector,
     diplomat: diplomat,
     argentina: argentina,
+    nuke: nuke,
+    policeman: policeman,
+    scientist: scientist,
+    guerilla: guerrilla,
     countryBack: countryBack,
     brazil: brazil,
+    spy: spy,
     china: china,
     defconOne: defconOne,
     defconTwo: defconTwo,
